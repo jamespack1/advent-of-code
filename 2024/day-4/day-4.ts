34c0d6d776ab932f6a4fa9370e8ca49610115b3b
James Pack <james.pack@sky.uk>
2024-12-05 22:33:21 +0000

    day 4 - part 2

--- 2024/day-4/day-4.ts
+++ 2024/day-4/day-4.ts
@@ -1,65 +1,117 @@
-import { readFile, sum } from "../utils.ts";
+import { readFile } from "../utils.ts";
 
-const findWordOnRow = (word: string) => {
-  const regex = new RegExp(word, "g");
-  return (row: string) => {
-    return [...row.matchAll(regex)].length;
-  };
-};
+type SearchSet = { pos: [x: number, y: number]; char: string }[];
 
-const rowsToColumns = (input: string[]) => {
-  const output: string[][] = [];
+const XMAS: SearchSet[] = [[
+  // SE
+  { pos: [0, 0], char: "X" },
+  { pos: [1, 1], char: "M" },
+  { pos: [2, 2], char: "A" },
+  { pos: [3, 3], char: "S" },
+], [
+  // S
+  { pos: [0, 0], char: "X" },
+  { pos: [0, 1], char: "M" },
+  { pos: [0, 2], char: "A" },
+  { pos: [0, 3], char: "S" },
+], [
+  // SW
+  { pos: [0, 0], char: "X" },
+  { pos: [-1, 1], char: "M" },
+  { pos: [-2, 2], char: "A" },
+  { pos: [-3, 3], char: "S" },
+], [
+  // W
+  { pos: [0, 0], char: "X" },
+  { pos: [-1, 0], char: "M" },
+  { pos: [-2, 0], char: "A" },
+  { pos: [-3, 0], char: "S" },
+], [
+  // NW
+  { pos: [0, 0], char: "X" },
+  { pos: [-1, -1], char: "M" },
+  { pos: [-2, -2], char: "A" },
+  { pos: [-3, -3], char: "S" },
+], [
+  // N
+  { pos: [0, 0], char: "X" },
+  { pos: [0, -1], char: "M" },
+  { pos: [0, -2], char: "A" },
+  { pos: [0, -3], char: "S" },
+], [
+  // NE
+  { pos: [0, 0], char: "X" },
+  { pos: [1, -1], char: "M" },
+  { pos: [2, -2], char: "A" },
+  { pos: [3, -3], char: "S" },
+], [
+  // E
+  { pos: [0, 0], char: "X" },
+  { pos: [1, 0], char: "M" },
+  { pos: [2, 0], char: "A" },
+  { pos: [3, 0], char: "S" },
+]];
+
+const CROSS_MAS: SearchSet[] = [
+  [
+    { pos: [0, 0], char: "A" },
+    { pos: [-1, -1], char: "M" },
+    { pos: [1, -1], char: "M" },
+    { pos: [-1, 1], char: "S" },
+    { pos: [1, 1], char: "S" },
+  ],
+  [
+    { pos: [0, 0], char: "A" },
+    { pos: [-1, -1], char: "M" },
+    { pos: [-1, 1], char: "M" },
+    { pos: [1, -1], char: "S" },
+    { pos: [1, 1], char: "S" },
+  ],
+  [
+    { pos: [0, 0], char: "A" },
+    { pos: [-1, -1], char: "S" },
+    { pos: [1, -1], char: "S" },
+    { pos: [-1, 1], char: "M" },
+    { pos: [1, 1], char: "M" },
+  ],
+  [
+    { pos: [0, 0], char: "A" },
+    { pos: [-1, -1], char: "S" },
+    { pos: [-1, 1], char: "S" },
+    { pos: [1, -1], char: "M" },
+    { pos: [1, 1], char: "M" },
+  ],
+];
+
+const searchFound = (
+  input: string[],
+  position: [x: number, y: number],
+) =>
+(searchSet: SearchSet) =>
+  searchSet.every((s) =>
+    input[position[0] - s.pos[0]]?.charAt(position[1] - s.pos[1]) === s.char
+  );
+
+const wordSearch = (
+  input: string[],
+  searchArray: { pos: [x: number, y: number]; char: string }[][],
+) => {
+  let found = 0;
   for (let i = 0; i < input[0].length; i++) {
     for (let j = 0; j < input.length; j++) {
-      output[j] = output[j] ?? [];
-      output[j].push(input[i][j] || "");
+      found += searchArray.filter(searchFound(input, [i, j])).length;
     }
   }
-
-  return output.map((row) => row.join(""));
+  return found;
 };
-
-const rowsToDiagonals = (input: string[]) => {
-  const output: string[][] = [];
-
-  for (let i = 0; i < (input.length + input[0].length); i++) {
-    for (let j = i; j >= 0; j--) {
-
-      if (i >= 0 && j >= 0) {
-        output[i] = output[i] ?? [];
-        output[i].push(input[j]?.[i-j] || "");
-      }
-    }
-  }
-
-  return output.map((row) => row.join(""));
-};
-
-const reverse = (str: string) =>
-  [...str].toReversed().join('');
 
 export const day4 = () => {
   console.log("Day 3");
   const input = readFile("./day-4/input.txt");
 
-  const asColumns = rowsToColumns(input);
-  const diagonals = rowsToDiagonals(input);
-  const diagonalRev = rowsToDiagonals(input.map(reverse));
+  const part1 = wordSearch(input, XMAS);
+  console.log("Part 1:", part1);
 
-  const part1 = [
-    input.map(findWordOnRow("XMAS")),
-    input.map(findWordOnRow("SAMX")),
-    asColumns.map(findWordOnRow("XMAS")),
-    asColumns.map(findWordOnRow("SAMX")),
-    diagonals.map(findWordOnRow("XMAS")),
-    diagonals.map(findWordOnRow("SAMX")),
-    diagonalRev.map(findWordOnRow("XMAS")),
-    diagonalRev.map(findWordOnRow("SAMX")),
-  ].map(r => r.reduce(sum, 0))
-
-
-  console.log("Part 1:", part1.reduce(sum, 0));
-
-  const part2 = 0;
+  const part2 = wordSearch(input, CROSS_MAS);
   console.log("Part 2:", part2);
 };
